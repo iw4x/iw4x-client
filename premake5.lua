77df2fcae8f0d1d2cabe6a56466ad3ae3336fcec
/dev/urandom <iw4x-dev-urandom@protonmail.com>
2015-12-31 02:44:04 +0100

    --copy-to option for premake5 to define custom folder to copy iw4x.dll to after build.

--- premake5.lua
+++ premake5.lua
@@ -1,3 +1,9 @@
+-- Option to allow copying the DLL file to a custom folder after build
+newoption {
+	trigger = "copy-to",
+	description = "Optionally copy the DLL to a custom folder after build, define the path here if wanted."
+}
+
 solution "iw4x"
 	location ("./build")
 	configurations { "Normal" }
@@ -11,3 +17,10 @@ solution "iw4x"
 		configuration "Normal"
 			defines { "NDEBUG" }
 			flags { "Optimize", "MultiProcessorCompile", "Symbols" }
+
+		if _OPTIONS["copy-to"] then
+			saneCopyToPath = string.gsub(_OPTIONS["copy-to"] .. "\\", "\\\\", "\\")
+			postbuildcommands {
+				"copy /y \"$(TargetPath)\" \"" .. saneCopyToPath .. "\""
+			}
+		end
