5fb564408b258eb77b80f6dbaf2ddf830cb1dcd2
momo5502 <mauriceheumann@gmail.com>
2016-01-03 04:13:35 +0100

    Fix party prematch.

--- src/Components/Modules/Party.cpp
+++ src/Components/Modules/Party.cpp
@@ -173,6 +173,12 @@ namespace Components
 		// Enable XP Bar
 		Utils::Hook(0x62A2A7, Party::UIDvarIntStub, HOOK_CALL).Install()->Quick();
 
+		// Set NAT to open
+		Utils::Hook::Set<int>(0x79D898, 1);
+
+		// Disable host migration
+		Utils::Hook::Set<BYTE>(0x5B58B2, 0xEB);
+
 		// Fix xstartlobby
 		//Utils::Hook::Set<BYTE>(0x5B71CD, 0xEB);
 
