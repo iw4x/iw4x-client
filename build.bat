7f1895f530bb969039dc4eaea53e494e8d3bac68
/dev/urandom <iw4x-dev-urandom@protonmail.com>
2016-01-03 15:12:57 +0100

    Here's a useful, easy and nicely looking build.bat script!

---
+++ build.bat
@@ -0,0 +1,26 @@
+@echo off & setlocal
+if exist "%VS140COMNTOOLS%\vsvars32.bat" call "%VS140COMNTOOLS%\vsvars32.bat"
+msbuild /version >NUL 2>NUL
+if errorlevel 0 goto:build
+
+if exist "%VS120COMNTOOLS%\vsvars32.bat" call "%VS120COMNTOOLS%\vsvars32.bat"
+msbuild /version >NUL 2>NUL
+if errorlevel 0 goto:build
+
+if exist "%PROGRAMFILES(x86)%\msbuild\14.0\Bin\msbuild.exe" path %PROGRAMFILES(x86)%\msbuild\14.0\Bin;%PATH%
+msbuild /version >NUL 2>NUL
+if errorlevel 0 goto:build
+
+if exist "%PROGRAMFILES(x86)%\msbuild\12.0\Bin\msbuild.exe" path %PROGRAMFILES(x86)%\msbuild\12.0\Bin;%PATH%
+msbuild /version >NUL 2>NUL
+if errorlevel 0 goto:build
+
+echo Couldn't find any MSBuild to build this project.
+echo Make sure you have Microsoft Build Tools 2013 or 2015 or Visual Studio 2013 or 2015 installed.
+endlocal
+exit /B 1
+
+:build
+msbuild /nologo /m /v:m /p:Configuration=Normal /p:Platform=Win32 build\iw4x.sln
+endlocal
+exit /B %ERRORLEVEL%
