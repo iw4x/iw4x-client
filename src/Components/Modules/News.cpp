b00f191c7d89e0460f79c71abe3de754294fa69c
sr0 <sr0@iw4xcachep26muba.onion>
2016-01-08 00:59:59 +0100

    changelog

--- src/Components/Modules/News.cpp
+++ src/Components/Modules/News.cpp
@@ -12,11 +12,13 @@ namespace Components
 	News::News()
 	{
 		News::Motd = Utils::WebIO("IW4x", "http://localhost/iw4/motd.txt").Get();
-
+		
 		if (!News::Motd.size())
 		{
 			News::Motd = "Welcome to ReactIW4x Multiplayer!";
 		}
+
+		Localization::Set("MPUI_CHANGELOG_TEXT", Utils::WebIO("IW4x", "http://localhost/iw4/changelog.txt").Get().data());
 
 		// Patch motd setting
 		Utils::Hook(0x60BF19, News::GetMotd, HOOK_CALL).Install()->Quick();
