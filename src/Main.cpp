77be8fb151af7c653f8c5164e2964b44970e9024
momo5502 <mauriceheumann@gmail.com>
2016-01-24 13:54:36 +0100

    Comments.

--- src/Main.cpp
+++ src/Main.cpp
@@ -27,7 +27,9 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 		{
 			__asm
 			{
+				// This has to be called, otherwise the hook is not uninstalled and we're deadlocking
 				call Main::Initialize
+
 				mov eax, 6BAC0Fh
 				jmp eax
 			}
