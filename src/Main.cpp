7db2956e458cc35f206dc5608f726c50f6908059
momo5502 <mauriceheumann@gmail.com>
2016-01-24 13:44:42 +0100

    Fix entry point.

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,7 +6,7 @@ namespace Main
 
 	void Initialize()
 	{
-		EntryPointHook.Uninstall();
+		Main::EntryPointHook.Uninstall();
 		Components::Loader::Initialize();
 	}
 
@@ -14,30 +14,30 @@ namespace Main
 	{
 		Components::Loader::Uninitialize();
 	}
+}
 
-	BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+	if (ul_reason_for_call == DLL_PROCESS_ATTACH)
 	{
-		if (ul_reason_for_call == DLL_PROCESS_ATTACH)
-		{
-			DWORD oldProtect;
-			VirtualProtect(GetModuleHandle(NULL), 0x6C73000, PAGE_EXECUTE_READWRITE, &oldProtect);
+		DWORD oldProtect;
+		VirtualProtect(GetModuleHandle(NULL), 0x6C73000, PAGE_EXECUTE_READWRITE, &oldProtect);
 
-			EntryPointHook.Initialize(0x6BAC0F, [] ()
+		Main::EntryPointHook.Initialize(0x6BAC0F, [] ()
+		{
+			__asm
 			{
-				__asm
-				{
-					call Main::Initialize
-					mov eax, 6BAC0Fh
-					jmp eax
-				}
+				call Main::Initialize
+				mov eax, 6BAC0Fh
+				jmp eax
+			}
 
-			})->Install();
-		}
-		else if (ul_reason_for_call == DLL_PROCESS_DETACH)
-		{
-			Main::Uninitialize();
-		}
-
-		return TRUE;
+		})->Install();
 	}
+	else if (ul_reason_for_call == DLL_PROCESS_DETACH)
+	{
+		Main::Uninitialize();
+	}
+
+	return TRUE;
 }
