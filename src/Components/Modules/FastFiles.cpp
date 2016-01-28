23b8a60765e4270bbab8c54448e7fd0d84df0a6c
momo5502 <mauriceheumann@gmail.com>
2016-01-28 17:49:33 +0100

    Don't load custom fastfiles for zonebuilder.

--- src/Components/Modules/FastFiles.cpp
+++ src/Components/Modules/FastFiles.cpp
@@ -133,8 +133,11 @@ namespace Components
 		Utils::Hook(0x44DA90, FastFiles::GetZoneLocation, HOOK_JUMP).Install()->Quick();
 
 		// Allow dlc ui zone loading
-		Utils::Hook(0x506BC7, FastFiles::LoadInitialZones, HOOK_CALL).Install()->Quick();
-		Utils::Hook(0x60B4AC, FastFiles::LoadDLCUIZones, HOOK_CALL).Install()->Quick();
+		if (!ZoneBuilder::IsEnabled())
+		{
+			Utils::Hook(0x506BC7, FastFiles::LoadInitialZones, HOOK_CALL).Install()->Quick();
+			Utils::Hook(0x60B4AC, FastFiles::LoadDLCUIZones, HOOK_CALL).Install()->Quick();
+		}
 
 		// basic checks (hash jumps, both normal and playlist)
 		Utils::Hook::Nop(0x5B97A3, 2);
