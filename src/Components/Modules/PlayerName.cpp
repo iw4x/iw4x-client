7ed67debfc4d19b33912a01d4abd1170a704aa00
momo5502 <mauriceheumann@gmail.com>
2016-09-05 19:55:47 +0200

    Start working on issue

---
+++ src/Components/Modules/PlayerName.cpp
@@ -0,0 +1,33 @@
+#include "STDInclude.hpp"
+
+namespace Components
+{
+	std::string PlayerName::PlayerNames[18];
+
+	int PlayerName::GetClientName(int /*localClientNum*/, int index, char *buf, int size)
+	{
+		if (index < 0 || index >= 18) return 0;
+		return strncpy_s(buf, size, PlayerName::PlayerNames[index].data(), PlayerName::PlayerNames[index].size()) == 0;
+	}
+
+	PlayerName::PlayerName()
+	{
+		if (0)
+		{
+			for (int i = 0; i < ARRAY_SIZE(PlayerName::PlayerNames); ++i)
+			{
+				PlayerName::PlayerNames[i] = "mumu";
+			}
+
+			Utils::Hook(Game::CL_GetClientName, PlayerName::GetClientName, HOOK_JUMP).Install()->Quick();
+		}
+	}
+
+	PlayerName::~PlayerName()
+	{
+		for (int i = 0; i < ARRAY_SIZE(PlayerName::PlayerNames); ++i)
+		{
+			PlayerName::PlayerNames[i].clear();
+		}
+	}
+}
---
+++ src/Components/Modules/PlayerName.hpp
@@ -0,0 +1,18 @@
+namespace Components
+{
+	class PlayerName : public Component
+	{
+	public:
+		PlayerName();
+		~PlayerName();
+
+#ifdef DEBUG
+		const char* GetName() { return "PlayerName"; };
+#endif
+
+	private:
+		static std::string PlayerNames[18];
+
+		static int GetClientName(int localClientNum, int index, char *buf, int size);
+	};
+}
