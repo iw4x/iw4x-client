18b2944d2ea84fef9c8168b40a151941646e4265
momo5502 <mauriceheumann@gmail.com>
2016-10-30 14:17:49 +0100

    Don't unprotect the main process

--- src/Main.cpp
+++ src/Main.cpp
@@ -56,10 +56,12 @@ BOOL APIENTRY DllMain(HMODULE /*hModule*/, DWORD  ul_reason_for_call, LPVOID /*l
 			return FALSE;
 		}
 
+
 		DWORD oldProtect;
 		std::uint8_t* module = reinterpret_cast<std::uint8_t*>(GetModuleHandle(NULL));
-		VirtualProtect(module, 0x6C73000, PAGE_EXECUTE_READWRITE, &oldProtect); // Unprotect the entire process
+		//VirtualProtect(module, 0x6C73000, PAGE_EXECUTE_READWRITE, &oldProtect); // Unprotect the entire process
 		VirtualProtect(module + 0x1000, 0x2D6000, PAGE_EXECUTE_READ, &oldProtect); // Protect the .text segment
+
 
 		Main::EntryPointHook.Initialize(0x6BAC0F, [] ()
 		{
