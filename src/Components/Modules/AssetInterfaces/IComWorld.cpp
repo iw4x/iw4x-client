7135ef2a606f5a7621cb1a469f8de4eb6a1908f9
momo5502 <mauriceheumann@gmail.com>
2016-12-31 03:46:23 +0100

    [IComWorld] Mark light definition

--- src/Components/Modules/AssetInterfaces/IComWorld.cpp
+++ src/Components/Modules/AssetInterfaces/IComWorld.cpp
@@ -2,6 +2,21 @@
 
 namespace Assets
 {
+	void IComWorld::mark(Game::XAssetHeader header, Components::ZoneBuilder::Zone* builder)
+	{
+		Game::ComWorld* asset = header.comWorld;
+		if (asset->lights)
+		{
+			for (int i = 0; i < asset->lightCount; ++i)
+			{
+				if (asset->lights[i].name)
+				{
+					builder->loadAsset(Game::XAssetType::ASSET_TYPE_LIGHT_DEF, std::string(asset->lights[i].name), false);
+				}
+			}
+		}
+	}
+
 	void IComWorld::save(Game::XAssetHeader header, Components::ZoneBuilder::Zone* builder)
 	{
 		AssertSize(Game::ComWorld, 16);
--- src/Components/Modules/AssetInterfaces/IComWorld.hpp
+++ src/Components/Modules/AssetInterfaces/IComWorld.hpp
@@ -6,5 +6,6 @@ namespace Assets
 		virtual Game::XAssetType getType() override { return Game::XAssetType::ASSET_TYPE_COMWORLD; };
 
 		virtual void save(Game::XAssetHeader header, Components::ZoneBuilder::Zone* builder) override;
+		virtual void mark(Game::XAssetHeader header, Components::ZoneBuilder::Zone* builder) override;
 	};
 }
