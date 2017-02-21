437ee48f8f603b22dc0d57c0e987298d7b4950d0
sr0 <sr0@iw4xcachep26muba.onion>
2017-02-21 21:39:40 +0100

    [ConnectProtocol] Do not initialize when running as a dedicated server

--- src/Components/Modules/ConnectProtocol.cpp
+++ src/Components/Modules/ConnectProtocol.cpp
@@ -204,6 +204,8 @@ namespace Components
 
 	ConnectProtocol::ConnectProtocol()
 	{
+		if (Dedicated::Dedicated::IsEnabled()) return;
+
 		// IPC handler
 		IPCPipe::On("connect", [] (std::string data)
 		{
