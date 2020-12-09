fcdfb563e693bf4edf6deb5dc0fc68acdb33392f
INeedBots <ineedbots@outlook.com>
2020-12-09 14:57:34 -0600

    [News] Remove broken updater

--- src/Components/Modules/News.hpp
+++ src/Components/Modules/News.hpp
@@ -6,26 +6,15 @@ namespace Components
 	{
 	public:
 		News();
-		~News();
 
 		void preDestroy() override;
 		bool unitTest() override;
 
-		static void LaunchUpdater(const std::string& params);
-		static bool Updating();
-
 	private:
-		static std::string UpdaterArgs;
-		static std::string UpdaterHash;
 		static std::thread Thread;
-		static std::mutex UpdaterMutex;
 
 		static bool Terminate;
-		static bool GetLatestUpdater();
 		static bool DownloadUpdater();
-
-		static void CheckForUpdate();
-		static void ExitProcessStub(unsigned int exitCode);
 
 		static const char* GetNewsText();
 	};
