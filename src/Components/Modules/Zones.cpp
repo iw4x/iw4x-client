49a2fc8b70b6496f4a48330aea5f585f4be09903
Rackover Xiaochad <Rackover@live.fr>
2021-02-07 11:54:15 +0100

    Fix 332+ maps aswell as 359+

--- src/Components/Modules/Zones.cpp
+++ src/Components/Modules/Zones.cpp
@@ -1655,20 +1655,7 @@ namespace Components
 				image->delayLoadPixels = image359.loaded;
 				image->name = image359.name;
 
-				// CODO makes use of additional enumerator values (9, 10, 11) that don't exist in IW4
-				// We have to translate them. 9 is for Reflection probes,  11 is for Compass,  10 is for Lightmap
-				switch (image->category)
-				{
-					case 9:
-						image->category = Game::ImageCategory::IMG_CATEGORY_AUTO_GENERATED;
-						break;
-					case 10:
-						image->category = Game::ImageCategory::IMG_CATEGORY_LIGHTMAP;
-						break;
-					case 11:
-						image->category = Game::ImageCategory::IMG_CATEGORY_LOAD_FROM_FILE;
-						break;
-				}
+				FixImageCategory(image);
 
 				// Used for later stuff
 				(&image->delayLoadPixels)[1] = image359.pad3[1];
@@ -1676,10 +1663,38 @@ namespace Components
 			else
 			{
 				std::memcpy(buffer + 28, buffer + (size - 4), 4);
+
+				Game::GfxImage* image = reinterpret_cast<Game::GfxImage*>(buffer);
+				FixImageCategory(image);
 			}
 		}
 
 		return result;
+	}
+
+	void Zones::FixImageCategory(Game::GfxImage* image) {
+		// CODO makes use of additional enumerator values (9, 10, 11) that don't exist in IW4
+		// We have to translate them. 9 is for Reflection probes,  11 is for Compass,  10 is for Lightmap
+		switch (image->category)
+		{
+			case 9:
+				image->category = Game::ImageCategory::IMG_CATEGORY_AUTO_GENERATED;
+				break;
+			case 10:
+				image->category = Game::ImageCategory::IMG_CATEGORY_LIGHTMAP;
+				break;
+			case 11:
+				image->category = Game::ImageCategory::IMG_CATEGORY_LOAD_FROM_FILE;
+				break;
+		}
+
+
+		if (image->category > 7 || image->category < 0) {
+
+#ifdef DEBUG
+			if (IsDebuggerPresent()) __debugbreak();
+#endif
+		}
 	}
 
 	bool Zones::LoadXAsset(bool atStreamStart, char* buffer, int size)
