5f7cefa5c95685af445f71de7b60f80285e729f8
Diavolo <edoardo.sanguineti222@gmail.com>
2021-07-18 18:11:20 +0200

    Fix Quit Hard

--- src/Utils/Library.cpp
+++ src/Utils/Library.cpp
@@ -2,9 +2,38 @@
 
 namespace Utils
 {
+	Library Library::load(const std::string& name)
+	{
+		return Library(LoadLibraryA(name.data()));
+	}
+
+	Library Library::load(const std::filesystem::path& path)
+	{
+		return Library::load(path.generic_string());
+	}
+
+	Library Library::get_by_address(void* address)
+	{
+		HMODULE handle = nullptr;
+		GetModuleHandleExA(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS, static_cast<LPCSTR>(address), &handle);
+		return Library(handle);
+	}
+
 	Library::Library(const std::string& buffer, bool _freeOnDestroy) : _module(nullptr), freeOnDestroy(_freeOnDestroy)
 	{
 		this->_module = LoadLibraryExA(buffer.data(), nullptr, 0);
+	}
+
+	Library::Library(const std::string& buffer)
+	{
+		this->_module = GetModuleHandleA(buffer.data());
+		this->freeOnDestroy = true;
+	}
+
+	Library::Library(const HMODULE handle)
+	{
+		this->_module = handle;
+		this->freeOnDestroy = true;
 	}
 
 	Library::~Library()
