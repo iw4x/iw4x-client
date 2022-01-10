0fe6a80429bbc543536805675902a4bf21df016d
FutureRave <edoardo.sanguineti222@gmail.com>
2022-01-10 15:28:37 +0000

    Refactor startup messages

--- src/Components/Modules/StartupMessages.cpp
+++ src/Components/Modules/StartupMessages.cpp
@@ -23,14 +23,20 @@ namespace Components
 				StartupMessages::TotalMessages = StartupMessages::MessageList.size();
 			}
 
-			std::string message = StartupMessages::MessageList.front();
-			StartupMessages::MessageList.pop_front();
+			const auto& message = StartupMessages::MessageList.front();
 
 			Game::Dvar_SetStringByName("ui_startupMessage", message.data());
 			Game::Dvar_SetStringByName("ui_startupMessageTitle", Utils::String::VA("Messages (%d/%d)", StartupMessages::TotalMessages - StartupMessages::MessageList.size(), StartupMessages::TotalMessages));
 			Game::Dvar_SetStringByName("ui_startupNextButtonText", StartupMessages::MessageList.size() ? "Next" : "Close");
 			Game::Cbuf_AddText(0, "openmenu startup_messages");
+
+			StartupMessages::MessageList.pop_front();
 		});
+	}
+
+	StartupMessages::~StartupMessages()
+	{
+		StartupMessages::MessageList.clear();
 	}
 
 	void StartupMessages::AddMessage(const std::string& message)
