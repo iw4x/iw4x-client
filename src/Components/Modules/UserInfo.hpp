fe0cbacdea433801fd5c74a6f415df650fca118c
Diavolo <edoardo.sanguineti222@gmail.com>
2022-06-04 18:52:46 +0200

    [UserInfo] Clear overrides when user disconnects

--- src/Components/Modules/UserInfo.hpp
+++ src/Components/Modules/UserInfo.hpp
@@ -7,14 +7,14 @@ namespace Components
 	public:
 		UserInfo();
 
+		static void ClearClientOverrides(int client);
+		static void ClearAllOverrides();
+
 	private:
 		using userInfoMap = std::unordered_map<std::string, std::string>;
 		static std::unordered_map<int, userInfoMap> UserInfoOverrides;
 
 		static void SV_GetUserInfo_Stub(int index, char* buffer, int bufferSize);
-
-		static void ClearClientOverrides(int client);
-		static void ClearAllOverrides();
 
 		static void AddScriptMethods();
 	};
