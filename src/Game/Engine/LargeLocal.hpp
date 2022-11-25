a4946981a1f60af11e475ce6aeb20fedb4e09721
FutureRave <edoardo.sanguineti222@gmail.com>
2022-11-25 13:45:23 +0000

    [LargeLocal]: Tabs vs Spaces

--- src/Game/Engine/LargeLocal.hpp
+++ src/Game/Engine/LargeLocal.hpp
@@ -2,30 +2,30 @@
 
 namespace Game::Engine
 {
-    class LargeLocal
-    {
-    public:
-        explicit LargeLocal(int sizeParam);
-        ~LargeLocal();
+	class LargeLocal
+	{
+	public:
+		explicit LargeLocal(int sizeParam);
+		~LargeLocal();
 
-        LargeLocal(LargeLocal&&) = delete;
-        LargeLocal(const LargeLocal&) = delete;
-        LargeLocal& operator=(LargeLocal&&) = delete;
-        LargeLocal& operator=(const LargeLocal&) = delete;
+		LargeLocal(LargeLocal&&) = delete;
+		LargeLocal(const LargeLocal&) = delete;
+		LargeLocal& operator=(LargeLocal&&) = delete;
+		LargeLocal& operator=(const LargeLocal&) = delete;
 
-        [[nodiscard]] void* GetBuf() const;
+		[[nodiscard]] void* GetBuf() const;
 
-    private:
-        void PopBuf();
+	private:
+		void PopBuf();
 
-        int startPos;
-        int size;
-    };
+		int startPos;
+		int size;
+	};
 
-    extern void LargeLocalEnd(int startPos);
-    extern void LargeLocalEndRight(int startPos);
+	extern void LargeLocalEnd(int startPos);
+	extern void LargeLocalEndRight(int startPos);
 
-    extern void* LargeLocalGetBuf(int startPos, int size);
+	extern void* LargeLocalGetBuf(int startPos, int size);
 
-    extern int CanUseServerLargeLocal();
+	extern int CanUseServerLargeLocal();
 }
