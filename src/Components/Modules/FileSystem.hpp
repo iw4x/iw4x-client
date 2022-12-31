dcf701562fa88326c4142cd88a17ede52de48dea
Edo <edoardo.sanguineti222@gmail.com>
2022-12-31 15:09:11 +0100

    Fix FS a bit (#688)

--- src/Components/Modules/FileSystem.hpp
+++ src/Components/Modules/FileSystem.hpp
@@ -10,9 +10,9 @@ namespace Components
 		public:
 			virtual ~AbstractFile() = default;
 
-			virtual bool exists() = 0;
-			virtual std::string getName() = 0;
-			virtual std::string& getBuffer() = 0;
+			[[nodiscard]] virtual bool exists() const noexcept = 0;
+			[[nodiscard]] virtual std::string getName() const = 0;
+			[[nodiscard]] virtual std::string& getBuffer() = 0;
 
 			virtual explicit operator bool()
 			{
@@ -27,9 +27,9 @@ namespace Components
 			File(std::string file) : filePath{std::move(file)} { this->read(); }
 			File(std::string file, Game::FsThread thread) : filePath{std::move(file)} { this->read(thread); }
 
-			bool exists() override { return !this->buffer.empty(); }
-			std::string getName() override { return this->filePath; }
-			std::string& getBuffer() override { return this->buffer; }
+			[[nodiscard]] bool exists() const noexcept override { return !this->buffer.empty(); }
+			[[nodiscard]] std::string getName() const override { return this->filePath; }
+			[[nodiscard]] std::string& getBuffer() override { return this->buffer; }
 
 		private:
 			std::string filePath;
@@ -44,9 +44,9 @@ namespace Components
 			RawFile() = default;
 			RawFile(std::string file) : filePath(std::move(file)) { this->read(); }
 
-			bool exists() override { return !this->buffer.empty(); }
-			std::string getName() override { return this->filePath; }
-			std::string& getBuffer() override { return this->buffer; }
+			[[nodiscard]] bool exists() const noexcept override { return !this->buffer.empty(); }
+			[[nodiscard]] std::string getName() const override { return this->filePath; }
+			[[nodiscard]] std::string& getBuffer() override { return this->buffer; }
 
 		private:
 			std::string filePath;
@@ -58,16 +58,16 @@ namespace Components
 		class FileReader
 		{
 		public:
-			FileReader() : handle(0), size(-1), name() {}
-			FileReader(const std::string& file);
+			FileReader() : handle(0), size(-1) {}
+			FileReader(std::string file);
 			~FileReader();
 
-			bool exists();
-			std::string getName();
-			std::string getBuffer();
-			int getSize();
-			bool read(void* buffer, size_t size);
-			void seek(int offset, int origin);
+			[[nodiscard]] bool exists() const noexcept;
+			[[nodiscard]] std::string getName() const;
+			[[nodiscard]] std::string getBuffer() const;
+			[[nodiscard]] int getSize() const noexcept;
+			bool read(void* buffer, std::size_t size) const noexcept;
+			void seek(int offset, int origin) const;
 
 		private:
 			int handle;
@@ -81,7 +81,7 @@ namespace Components
 			FileWriter(std::string file, bool append = false) : handle(0), filePath(std::move(file)) { this->open(append); }
 			~FileWriter() { this->close(); }
 
-			void write(const std::string& data);
+			void write(const std::string& data) const;
 
 		private:
 			int handle;
@@ -115,10 +115,10 @@ namespace Components
 		static int ExecIsFSStub(const char* execFilename);
 
 		static void FsStartupSync(const char* a1);
-		static void FsRestartSync(int a1, int a2);
-		static void FsShutdownSync(int a1);
+		static void FsRestartSync(int localClientNum, int checksumFeed);
+		static void FsShutdownSync(int closemfp);
 		static void DelayLoadImagesSync();
-		static int LoadTextureSync(Game::GfxImageLoadDef **loadDef, Game::GfxImage *image);
+		static int LoadTextureSync(Game::GfxImageLoadDef** loadDef, Game::GfxImage* image);
 
 		static void IwdFreeStub(Game::iwd_t* iwd);
 
