9e8a2b406276b0ed2d09b36daae8aa324a24577c
Edo <edoardo.sanguineti222@gmail.com>
2023-12-25 00:39:39 +0100

    Update Cache.hpp

--- src/Utils/Cache.hpp
+++ src/Utils/Cache.hpp
@@ -5,12 +5,11 @@ namespace Utils
 	class Cache
 	{
 	public:
-		static const char* Urls[];
-
 		static std::string GetUrl(const std::string& url, const std::string& path);
 		static std::string GetFile(const std::string& path, int timeout = 5000, const std::string& useragent = "IW4x");
 
 	private:
+		static const char* Urls[];
 		static std::mutex CacheMutex;
 		static std::string ValidUrl;
 	};
