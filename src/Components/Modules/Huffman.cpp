da76c99f04deedaf4dc098d81ffdf9e3effaf1bf
Caball <Caball009@al.al>
2024-05-14 03:35:52 +0200

    Removed unary plus from lambda hooks.
    IntelliSense made me think the hooking library wasn't able to handle lambdas, but that's not the case.

--- src/Components/Modules/Huffman.cpp
+++ src/Components/Modules/Huffman.cpp
@@ -5,16 +5,16 @@ namespace Components
 {
 	Huffman::Huffman()
 	{
-		Utils::Hook(0x414D92, +[](const unsigned char* from, unsigned char* to, int fromSize) // SV_ExecuteClientMessage
+		Utils::Hook(0x414D92, [](const unsigned char* from, unsigned char* to, int fromSize) // SV_ExecuteClientMessage
 			{ return Utils::Huffman::Decompress(from, to, fromSize, 0x800); }, HOOK_CALL).install()->quick();
-		Utils::Hook(0x4A9F56, +[](const unsigned char* from, unsigned char* to, int fromSize) // CL_ParseServerMessage
+		Utils::Hook(0x4A9F56, [](const unsigned char* from, unsigned char* to, int fromSize) // CL_ParseServerMessage
 			{ return Utils::Huffman::Decompress(from, to, fromSize, 0x20000); }, HOOK_CALL).install()->quick();
 
-		Utils::Hook(0x411C16, +[](bool, const unsigned char* from, unsigned char* to, int fromSize) // CL_WritePacket
+		Utils::Hook(0x411C16, [](bool, const unsigned char* from, unsigned char* to, int fromSize) // CL_WritePacket
 			{ return Utils::Huffman::Compress(from, to, fromSize, 0x800); }, HOOK_CALL).install()->quick();
-		Utils::Hook(0x5A85A1, +[](bool, const unsigned char* from, unsigned char* to, int fromSize) // CL_Record_f
+		Utils::Hook(0x5A85A1, [](bool, const unsigned char* from, unsigned char* to, int fromSize) // CL_Record_f
 			{ return Utils::Huffman::Compress(from, to, fromSize, 0x20000); }, HOOK_CALL).install()->quick();
-		Utils::Hook(0x48FEDD, +[](bool, const unsigned char* from, unsigned char* to, int fromSize) // SV_SendMessageToClient
+		Utils::Hook(0x48FEDD, [](bool, const unsigned char* from, unsigned char* to, int fromSize) // SV_SendMessageToClient
 			{ return Utils::Huffman::Compress(from, to, fromSize, 0x20000); }, HOOK_CALL).install()->quick();
 	}
 
