c58ed22224538b1e1eb0c7fa5c4339b5919d7abe
mxve <68632137+mxve@users.noreply.github.com>
2025-06-06 08:25:03 +0200

    revert #166 and restore m_rawinput functionality

--- src/Game/Functions.cpp
+++ src/Game/Functions.cpp
@@ -225,8 +225,6 @@ namespace Game
 	IN_RecenterMouse_t IN_RecenterMouse = IN_RecenterMouse_t(0x463D80);
 
 	IN_MouseMove_t IN_MouseMove = IN_MouseMove_t(0x64C490);
-	IN_MouseEvent_t IN_MouseEvent = IN_MouseEvent_t(0x4C84D0);
-	IN_Frame_t IN_Frame = IN_Frame_t(0x475E10);
 	IN_Init_t IN_Init = IN_Init_t(0x45D620);
 	IN_Shutdown_t IN_Shutdown = IN_Shutdown_t(0x426360);
 
--- src/Game/System.cpp
+++ src/Game/System.cpp
@@ -26,14 +26,6 @@ namespace Game
 	Sys_OutOfMemErrorInternal_t Sys_OutOfMemErrorInternal = Sys_OutOfMemErrorInternal_t(0x4B2E60);
 	Sys_QuitAndStartProcess_t Sys_QuitAndStartProcess = Sys_QuitAndStartProcess_t(0x45FCF0);
 
-	Sys_QueEvent_t Sys_QueEvent = Sys_QueEvent_t(0x497DE0);
-
-	void Sys_QueEvents(int time, int type, int value2, int ptrLength, void* ptr)
-	{
-		Sys_QueEvent(time, type, value2, TRUE, ptrLength, ptr);
-		Sys_QueEvent(time, type, value2, FALSE, ptrLength, ptr);
-	}
-
 	char(*sys_exitCmdLine)[1024] = reinterpret_cast<char(*)[1024]>(0x649FB68);
 	char(*sys_cmdline)[1024] = reinterpret_cast<char(*)[1024]>(0x649F760);
 
