42534f8dc3078acd54fe752d494b7ee89e89f947
shawn <159967382+neoXclusive@users.noreply.github.com>
2026-01-29 20:40:23 -0800

    removal of setanim actually

--- src/Components/Modules/ViewModelFx.cpp
+++ src/Components/Modules/ViewModelFx.cpp
@@ -41,7 +41,7 @@ namespace Components::ViewModelFxSetup
 			if (!Game::DObjGetBoneIndex(reinterpret_cast<int>(dobj), tagName, &boneIdx))
 			{
 				Game::Scr_Error(Utils::String::VA(
-					"PlayViewmodelFX(): clientNum '%s' does not have bone '%s'",
+					"PlayViewmodelFX(): clientNum '%d' does not have bone '%s'",
 					clientNum,
 					Game::SL_ConvertToString(tagName)
 				));
@@ -90,19 +90,6 @@ namespace Components::ViewModelFxSetup
 				Game::CG_StopBoltedEffect(0, 0, tpDObj->entnum, tagName);
 			}
 		});
-
-		GSC::Script::AddMethod("setanim", [](Game::scr_entref_t entref)
-		{
-			auto* ent = GSC::Script::Scr_GetPlayerEntity(entref);
-			if (!ent)
-				return;
-
-			int anim = Game::Scr_GetInt(0);
-
-			for (int i = 0; i < 2; i++)
-				ent->client->ps.weapState[i].weapAnim = anim;
-		});
-	}
 
 	Setup::Setup()
 	{
