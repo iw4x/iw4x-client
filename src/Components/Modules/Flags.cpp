6dba5b45086fa193d46ea28a7f27983cd48065fa
William Roy <wroy@proton.me>
2026-03-21 10:05:34 -0400

    Strip trailing quotes from command line flags
    Previously, passing arguments like "+set fs_game mods/mp_bots" would end
    up  being interpreted with a trailing quote included (e.g., "+set
    fs_game mods/mp_bots""), causing mods to fail to load.

--- src/Components/Modules/Flags.cpp
+++ src/Components/Modules/Flags.cpp
@@ -19,38 +19,86 @@ namespace Components
 		return false;
 	}
 
-	void Flags::ParseFlags()
-	{
-		static auto flagsParsed = false;
-		if (flagsParsed)
-		{
-			return;
-		}
+  void Flags::ParseFlags ()
+  {
+    static bool p (false);
 
-		// Only parse flags once
-		flagsParsed = true;
-		int numArgs;
-		auto* const argv = CommandLineToArgvW(GetCommandLineW(), &numArgs);
+    if (p)
+      return;
 
-		if (argv)
-		{
-			for (auto i = 0; i < numArgs; ++i)
-			{
-				std::wstring wFlag(argv[i]);
-				if (wFlag[0] == L'-')
-				{
-					wFlag.erase(wFlag.begin());
-					EnabledFlags.emplace_back(Utils::String::Convert(wFlag));
-				}
-			}
+    p = true;
 
-			LocalFree(argv);
-		}
+		// Note that the engine's parser mishandles trailing quotes. So here we
+		// strip them all globally from the raw OS buffers by shifting characters
+		// in-place. We cannot just replace them with spaces because that would
+		// naturally break space-separated values.
+		//
+    if (char* b = GetCommandLineA ())
+    {
+      char* r (b);
+      char* w (b);
 
-		// Workaround for wine
-		if (Utils::IsWineEnvironment() && Dedicated::IsEnabled() && !HasFlag("console") && !HasFlag("stdout"))
-		{
-			EnabledFlags.emplace_back("stdout");
-		}
-	}
+      while (*r != '\0')
+      {
+        if (*r != '\"')
+          *w++ = *r;
+
+        r++;
+      }
+
+      *w = '\0';
+    }
+
+    if (wchar_t * b = GetCommandLineW ())
+    {
+      wchar_t* r (b);
+      wchar_t* w (b);
+
+      while (*r != L'\0')
+      {
+        if (*r != L'\"')
+          *w++ = *r;
+
+        r++;
+      }
+
+      *w = L'\0';
+    }
+
+    int c (0);
+
+		// Parse the arguments. Since we already removed the quotes from the OS
+		// buffer above, CommandLineToArgvW() will parse the resulting clean
+		// string. This is perfectly safe as we are only looking for the '-'
+		// prefix anyway.
+		//
+    const auto a (CommandLineToArgvW (GetCommandLineW (), &c));
+
+    if (a)
+    {
+      for (int i (0); i < c; ++i)
+      {
+        std::wstring f (a[i]);
+
+        if (f[0] == L'-')
+        {
+          f.erase (f.begin ());
+          EnabledFlags.emplace_back (Utils::String::Convert (f));
+        }
+      }
+
+      LocalFree (a);
+    }
+
+		// Work around a Wine issue. If we are running dedicated and they did not
+		// specify output channels, force stdout so we actually see the logs.
+		//
+		if (Utils::IsWineEnvironment () &&
+      	Dedicated::IsEnabled ()     &&
+				!HasFlag ("console")        &&
+				!HasFlag ("stdout"))
+    {
+      EnabledFlags.emplace_back ("stdout");
+    }
+  }
 }
