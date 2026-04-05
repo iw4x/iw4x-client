5faf61ebd3b47614a5745d0802e97cd67b8ecb70
fieldhack <fieldhack@fieldhack.com>
2026-04-04 22:06:52 -0500

    fix: adjusted offset syntax and modified to only remove archive flag

--- src/Components/Modules/Dvar.cpp
+++ src/Components/Modules/Dvar.cpp
@@ -444,10 +444,10 @@ namespace Components
 		Utils::Hook::Xor<std::uint32_t>(0x4D3786, Game::DVAR_ARCHIVE);
 
 		// remove all flags for r_drawSun
-		Utils::Hook::Set<std::uint8_t>(0x51a239, Game::DVAR_NONE);
+		Utils::Hook::Xor<std::uint8_t>(0x51A239, Game::DVAR_ARCHIVE);
 
 		// remove all flags for r_specular
-		Utils::Hook::Set<std::uint8_t>(0x519c39, Game::DVAR_NONE);
+		Utils::Hook::Xor<std::uint8_t>(0x519C39, Game::DVAR_ARCHIVE);
 
 		// remove write protection from fs_game
 		Utils::Hook::Xor<std::uint32_t>(0x6431EA, Game::DVAR_INIT);
